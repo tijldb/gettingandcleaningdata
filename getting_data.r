b4be19be0b1ccc06ebbeafba311468d336c1449d
tijldb <debacker.tijl@gmail.com>
2014-07-21 16:38:09 +0200

    Getting Data
    Corrected getting data script
    unzips into the current directory

--- getting_data.r
+++ getting_data.r
@@ -9,5 +9,5 @@
 # download file
 download.file('https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip',destfile='ds.zip')
 
-# unzip this file (this creates a new subdirectory 'UCI HAR dataset' in the root folder of the project)
-unzip('ds.zip', junkpaths=TRUE)
+# unzip this file into the same directory
+unzip('ds.zip', junkpaths=FALSE)
